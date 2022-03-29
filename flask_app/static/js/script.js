d7558cf7db4732a10170c9bf9c214e51ab43c138
TheTechieYeti <elliott.scott.t@gmail.com>
2022-03-29 08:21:54 -0700

    Introduced Modals

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -4,7 +4,14 @@ function loading(params) {
     
 }
 function bring_to_top(params) {
-    document.getElementById("reg_wrapper").style.zIndex = "auto";
-    document.getElementById("login_wrapper").style.zIndex = "auto";
+    console.log('working')
+    document.getElementById("private_chat_form").style.zIndex = "auto";
+    document.getElementById("public_chat_form").style.zIndex = "auto";
     document.getElementById(params).style.zIndex=1;
 }
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
